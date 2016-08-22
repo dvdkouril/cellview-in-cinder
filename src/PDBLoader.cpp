21978f0af4d298df294921408e3829c6fcd5d1b8
David Kouřil <david.kouril@gmail.com>
2016-08-22 19:58:41 +0200

    Implement PDB loading and parsing

--- src/PDBLoader.cpp
+++ src/PDBLoader.cpp
@@ -35,16 +35,36 @@ Molecule * PDBLoader::loadFromFile(const char * path)
     
     std::fstream ifs(path, std::ifstream::in);
     string line;
+//    int i = 0;
     while (std::getline(ifs, line))
     {
         auto tokens = split(line);
         if (tokens[0] == "ATOM")
         {
+            string xStr = tokens[20];
+            string yStr = tokens[21];
+            string zStr = tokens[22];
             
+            double x = ::atof(xStr.c_str());
+            double y = ::atof(yStr.c_str());
+            double z = ::atof(zStr.c_str());
+            
+            Atom atom;
+            atom.position.x = x;
+            atom.position.y = y;
+            atom.position.z = z;
+            
+            mol->atoms.push_back(atom);
+//            if (i > 0) return nullptr;
+//            int tokenNumber = 0;
+//            for (auto string : tokens) {
+//                cout << tokenNumber << ": " << string << endl;
+//                tokenNumber += 1;
+//            }
+//            cout << endl;
+//            i += 1;
         }
     }
     
-    Atom a;
-    mol->atoms.push_back(a);
     return mol;
 }
